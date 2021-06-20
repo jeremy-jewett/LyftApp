537dbb81a1784ce2b55f7a6ab45f93685b5b3194
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2021-06-20 14:48:37 -0700

    Added files created in PyCharm

---
+++ ThirdLetter.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/'):
+def intro():
+    return 'this is the beginning'
+
+
+@app.route('/test')
+def every_third(word_to_cut):
+    solution = []
+    if len(word) < 3:
+        return ''
+    for i in word[2::3]:
+        solution.append(i)
+    return ''.join(solution)
+
+
