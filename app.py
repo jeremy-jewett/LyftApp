d0a3160e0c4203e91b9e865d10f85eed788bb163
Glitch (shimmering-garnet-juravenator) <none>
2021-06-21 00:00:49 +0000

    🐫🐝 Final working model

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+d = {}
 
 @app.route('/')
-def intro():
-    return 'this is the beginning'
+def home():
+  return """Jeremy's app is running!!"""
 
 
-@app.route('/test')
-def every_third(word_to_cut):
-    solution = []
-    if len(word) < 3:
-        return ''
-    for i in word[2::3]:
-        solution.append(i)
-    return ''.join(solution)
+@app.route('/test', methods=['POST'] )
+def every_third():
+  string_to_cut = request.values['string_to_cut']
+  solution = []
+  if len(string_to_cut) < 3:
+    return ''
+  for i in string_to_cut[2::3]:
+    solution.append(i)
+  return_string = ''.join(solution)
+  return jsonify({'return_string':return_string})
+
+if __name__ == "__main__":
+  app.run()
 
 
